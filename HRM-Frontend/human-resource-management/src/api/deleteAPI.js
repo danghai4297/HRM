214b1ed66d5b9e5469447911ed97c69b626ba080
DangHai <80934622+danghai4297@users.noreply.github.com>
2021-10-13 21:16:59 +0700

    file api put and delete

---
+++ HRM-Frontend/human-resource-management/src/api/deleteAPI.js
@@ -0,0 +1,112 @@
+import axiosClient from "./axiosClient";
+const DeleteApi = {
+  //delete dm chức danh
+  deleteDMCD: (id) => {
+    const url = `/DanhMucChucDanh/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete danh muc chức vụ
+  deleteDMCV: (id) => {
+    const url = `/DanhMucChucVu/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete danh muc chuyên môn
+  deleteDMCM: (id) => {
+    const url = `/DanhMucChuyenMon/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete danh mục dân tộc
+  deleteDMDT: (id) => {
+    const url = `/DanhMucDanToc/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm hôn nhân
+  deleteDMHN: (id) => {
+    const url = `/DanhMucHonNhan/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm khen thưởng
+  deleteDMKT: (id) => {
+    const url = `/DanhMucKhenThuongKyLuat/khen-thuong/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm kỷ luật
+  deleteDMKL: (id) => {
+    const url = `/DanhMucKhenThuongKyLuat/ky-luat/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm loại hợp đồng
+  deleteDMLHD: (id) => {
+    const url = `/DanhMucLoaiHopDong/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm ngạch công chức
+  deleteDMNCC: (id) => {
+    const url = `/DanhMucNgachCongChuc/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm ngoại ngữ
+  deleteDMNN: (id) => {
+    const url = `/DanhMucNgoaiNgu/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm người thân
+  deleteDMNT: (id) => {
+    const url = `/DanhMucNguoiThan/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm nhóm lương
+  deleteDMNL: (id) => {
+    const url = `/DanhMucNhomLuong/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm phòng ban
+  deleteDMPB: (id) => {
+    const url = `/DanhMucPhongBan/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm tính chất lao động
+  deleteDMTCLD: (id) => {
+    const url = `/DanhMucTinhChatLaoDong/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete danh mục tổ
+  deleteDMT: (id) => {
+    const url = `/DanhMucTo/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete danh mục tôn giáo
+  deleteDMTG: (id) => {
+    const url = `/DanhMucTonGiao/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm trình độ
+  deleteDMTD: (id) => {
+    const url = `/DanhMucTrinhDo/${id}`;
+    return axiosClient.delete(url);
+  },
+
+  //delete dm trình độ
+  deleteDMHTDT: (id) => {
+    const url = `/DanhMucHinhThucDaoTao/${id}`;
+    return axiosClient.delete(url);
+  },
+};
+
+export default DeleteApi;
